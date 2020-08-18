5f25ee909a3a14c2ad6d61a11255e16f1e157659
satishgupta07 <satishgupta.cse01@gmail.com>
2020-08-18 21:52:45 +0530

    Building My Works & Review (Made a Image Gallery)
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- js/app.js
+++ js/app.js
@@ -14,4 +14,57 @@ $(document).ready(function(){
         bar.style.width = percentage + '%';
     })
 
+
+    // image filter 
+
+    var $wrapper = $('.portfolio__wrapper');
+    // Initialize isotope 
+    $wrapper.isotope({
+        filter : '*',
+        layoutMode : 'masonry',
+        animationOptions : {
+            duration: 750,
+            easing: 'linear'
+        }
+    });
+
+    let links = document.querySelectorAll('.tabs a');
+
+    links.forEach(link => {
+
+        let selector = link.dataset.filter;
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            $wrapper.isotope({
+                filter : selector,
+                layoutMode : 'masonry',
+                animationOptions : {
+                    duration: 750,
+                    easing: 'linear'
+                }
+            });
+
+            links.forEach(link =>{
+                link.classList.remove('active');
+            })
+
+            e.target.classList.add('active');
+
+        });
+    })
+
+        // Magnify pop up 
+        console.log($('.magnify').magnificPopup)
+        $('.magnify').magnificPopup({
+         type: 'image',
+         gallery: {
+            enabled : true
+         },
+         zoom : {
+           enable: true
+         }
+        });
+
+
 });
