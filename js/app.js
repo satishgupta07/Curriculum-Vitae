3f5ee251f16e85aae9417871cda927619456b03b
satishgupta07 <satishgupta.cse01@gmail.com>
2020-08-18 17:44:42 +0530

    Build the Edu & Skill Section (Use timeline & progress bar)
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- js/app.js
+++ js/app.js
@@ -5,4 +5,13 @@ $(document).ready(function(){
         dropRadius:10
     });
 
+    const bars = document.querySelectorAll('.progress__bar');
+
+    bars.forEach(function(bar){
+        let percentage = bar.dataset.percent;
+        let tooltip = bar.children[0];
+        tooltip.innerText = percentage + '%';
+        bar.style.width = percentage + '%';
+    })
+
 });
